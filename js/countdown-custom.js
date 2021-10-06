4e21e4a69f833491dd3f3d72911eeac42afa70a0
kartikmadan <kartikmadan26@gmail.com>
2021-10-06 13:21:27 +0530

    updated hamburger

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2021, 12, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2021, 9, 12, 17,30)}); // year, month, date, hour
         });
 });		
 
