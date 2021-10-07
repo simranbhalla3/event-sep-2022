c34362829a23808e2473b7acc94a1169e2d95a28
Simran <simranbhalla0307@gmail.com>
2021-10-07 12:06:18 +0530

    updated farhan choudhary

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2021, 9, 7, 17,30)}); // year, month, date, hour
+        });
+});		
+
